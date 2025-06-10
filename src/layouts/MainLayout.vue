d56a97eaade7990ab68955811ddd94aefc0eb77a
Zee-RGB <134300176+zee-RGB@users.noreply.github.com>
2025-06-10 12:21:36 +1000

    feat: refactor navigation and entry components; remove EssentialLink.vue

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -24,15 +24,13 @@
       show-if-above
       bordered
       class="bg-primary"
+      :width="200"
+      :breakpoint="767"
     >
       <q-list>
-        <q-item-label header class="text-white"> Essential Links </q-item-label>
+        <q-item-label header class="text-white"> Navigation </q-item-label>
 
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <Navlink v-for="link in navLinks" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -43,51 +41,19 @@
 </template>
 
 <script setup>
-import EssentialLink from "components/EssentialLink.vue";
+import Navlink from "src/components/Nav/NavLink.vue";
 import { ref } from "vue";
 
-const linksList = [
+const navLinks = [
   {
-    title: "Docs",
-    caption: "quasar.dev",
-    icon: "school",
-    link: "https://quasar.dev",
+    title: "Entries",
+    icon: "savings",
+    link: "/",
   },
   {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
+    title: "Settings",
+    icon: "settings",
+    link: "/settings",
   },
 ];
 
--- src/pages/PageEntries.vue
+++ src/pages/PageEntries.vue
@@ -1,9 +1,55 @@
 <template>
-  <q-page class="flex flex-center column">
-    <h5>Page Entries</h5>
+  <q-page>
+    <div class="q-pa-md">
+      <q-list bordered separator>
+        <q-item v-for="entry in entries" :key="entry.id">
+          <q-item-section> {{ entry.name }} </q-item-section>
+
+          <q-item-section side> {{ entry.amount }} </q-item-section>
+        </q-item>
+
+        <q-item>
+          <q-item-section> Total $: </q-item-section>
+          <q-item-section side> {{ total }} </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
   </q-page>
 </template>
 
 <script setup>
-//
+import { ref } from "vue";
+
+const entries = ref([
+  {
+    id: "id: 1",
+    name: "Salary",
+    amount: 4000.0,
+  },
+
+  {
+    id: "id: 2",
+    name: "Rent",
+    amount: -999,
+  },
+
+  {
+    id: "id: 3",
+    name: "Phone",
+    amount: -120.0,
+  },
+  {
+    id: "id: 4",
+    name: "Groceries",
+    amount: -100.0,
+  },
+
+  {
+    id: "id: 5",
+    name: "Unknown",
+    amount: 0,
+  },
+]);
+
+const total = ref(entries.value.reduce((acc, entry) => acc + entry.amount, 0));
 </script>
